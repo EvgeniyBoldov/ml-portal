fcb6a0853b0f7b842ca73d26d8a5fbe0405707b0
EvgenyB <bea@gfn.team>
2026-01-21 22:45:48 +0300

    fix: укоротить имя миграции до 32 символов
    - Переименовать 0040_collection_vector_unification -> 0040_coll_vector_unif
    - Обновить revision ID в файле миграции
    - Фикс ошибки: value too long for type character varying(32)

--- apps/api/src/app/migrations/versions/0040_coll_vector_unif.py
+++ apps/api/src/app/migrations/versions/0040_coll_vector_unif.py
@@ -10,7 +10,7 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
-revision: str = '0040_collection_vector_unification'
+revision: str = '0040_coll_vector_unif'
 down_revision: Union[str, None] = '0039_agent_collections'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
